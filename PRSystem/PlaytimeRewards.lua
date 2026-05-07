5b3ac9ad1883e6dacfa9d78b766738a2ed9eeae4
RxDev <u9897612899@gmail.com>
2026-05-07 19:32:39 +0200

    Add PlaytimeRewards.lua with reward configurations

---
+++ PRSystem/PlaytimeRewards.lua
@@ -0,0 +1,26 @@
+return {
+	["FirstReward"]= {
+		Time = 60,
+		Coins = 5_000,
+	},
+
+	["SecondReward"]= {
+		Time = 300,
+		Coins = 10_000,
+	},
+
+	["ThirdReward"]= {
+		Time = 600,
+		Coins = 25_000,
+	},
+
+	["FourthReward"]= {
+		Time = 1_800,
+		Coins = 50_000,
+	},
+
+	["FifthReward"]= {
+		Time = 3_600,
+		Coins = 100_000,
+	},
+}
